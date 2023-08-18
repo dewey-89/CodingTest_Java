d6bd2d4ed8ce3c324b61255c568b96f273827883
dewey89 <140939516+dewey-89@users.noreply.github.com>
2023-08-18 20:07:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int xl = Integer.toString(x).length();
+        int sum = 0;
+        int temp = x;
+        while(temp!=0){
+            sum += temp%10;
+            temp/=10;
+        }
+        if(x%sum!=0){
+            answer = false;
+        }
+        return answer;
+    }
+}
